8fffddcbe478dc8b11b5f3f22ea6aaf94dada5c5
fariskt <fariskt00@gmail.com>
2025-02-24 23:14:49 +0530

    middleware issue again

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
+  console.log("ALL COOKIES:", req.cookies); // See if any cookies exist
   const token = req.cookies.get("token")?.value;
   console.log("middle ware ", token);
   
