ef4c0f581f5243a2368b54be35c01b5ec5c3941e
fariskt <fariskt00@gmail.com>
2025-05-26 21:55:58 +0530

    updated backend url

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
